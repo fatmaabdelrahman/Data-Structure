269524ec0cec7d3397da2f4d3a82aea3f224d4b6
fatmaabdelrahman <fabdelrahman72@yahoo.com>
2022-12-02 04:11:39 +0200

    queue with linked list

---
+++ queue/queue.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node* next;
+	Node()
+	{
+		data = 0;
+		next = NULL;
+	}
+
+};
+
+class Queue
+{
+public:
+	Node* front;
+	Node* rear;
+	Queue()
+	{
+		front = rear = NULL;
+	}
+
+	bool isEmpty()
+	{
+		if (front == NULL )//&& rear == NULL)
+			return true;
+		else
+			return false;
+	
+	}
+
+	void Enqueue(int item)
+	{
+		Node* newnode = new Node();
+		newnode->data = item;
+
+		if (isEmpty())
+			front = rear = newnode;
+		else
+		{
+			rear->next = newnode;
+			rear = newnode;
+		}
+	}
+	void display()
+	{
+		if (isEmpty())
+			cout << "Queue is Empty, no items to display \n";
+		else
+		{
+			Node* temp = front;
+			while (temp != NULL)
+			{
+				cout << temp->data << "\t";
+				temp = temp->next;
+			}
+			cout << endl;
+		}
+	}
+
+	int count()
+	{
+		int counter = 0;
+		Node* temp = front;
+		while (temp != NULL)
+		{
+			counter++;
+			temp = temp->next;
+		}
+		return counter;
+	}
+	bool isFound(int item)
+	{
+		bool found = false;
+		Node* temp = front;
+		while (temp != NULL)
+		{
+			if (temp->data == item)
+				found = true;
+			temp = temp->next;
+		}
+		return found;
+	}
+
+	int Dequeue()
+	{
+		int delvalue = -1;
+		if (isEmpty())
+			cout << "The queue is empty \n";
+		else if (front == rear)
+		{
+			delete front;
+			front = rear = NULL;
+		}
+		else
+		{
+			Node* delptr = front;
+			front = front->next;
+			delvalue = delptr->data;
+			delete delptr;
+		}
+		
+		return delvalue;
+	}
+
+	int getFront()
+	{
+		return front->data;
+	}
+
+	int getRear()
+	{
+		return rear->data;
+	}
+
+	void  Clear()
+	{
+		while (!isEmpty())
+		{
+			Dequeue();
+		}
+	}
+};
+
+
+int main()
+{
+	Queue q;
+	for (int i = 1; i <= 3; i++)
+	{
+		int item;
+		cout << "Enter Item to Enqueue \n";
+		cin >> item;
+		q.Enqueue(item);
+	}
+	q.display();
+
+	//q.Dequeue();
+	//cout << "After deque \n";
+	//q.display();
+	//q.Dequeue();
+	//q.display();
+	
+	cout << "Clear All items \n";
+	q.Clear();
+	cout << "Display after clear \n";
+	q.display();
+
+}
